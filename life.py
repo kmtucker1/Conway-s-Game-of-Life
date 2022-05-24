eaf7f63f043d12b848484a536a8a8f7b371c6003
kmtucker1 <kmtucker@mix.wvu.edu>
2022-05-23 21:07:42 -0400

    fexed bugs in life class

--- life.py
+++ life.py
@@ -1,83 +1,89 @@
+
 from xmlrpc.client import boolean
 
 
+class Life:
 
-#global variables for size of window
-cols: int = 10
-rows: int = 10
+    #global variables for size of window
+    cols: int = 10
+    rows: int = 10
 
-
-#current grid array
-current_grid: boolean = [[0]*cols]*rows
-
-
-#next grid array
-next_grid: boolean = [[0]*cols]*rows
+    #current grid
+    current_grid: boolean
 
 
-
-#interates through array grid and determines what the next grid will look like according to rules of game of life
-def create_next_grid(grid):
-    #recieves current grid and stores it
-    current_grid = grid
-
-    for i in range(len(current_grid)):
-        for j in range(len(current_grid[i])):
-            #number of neighbors cell has
-            neighbor_count: int = 0
-            #current status of cell
-            status: boolean = current_grid[i][j]
+    def __init__(self, cols: int, rows: int, current_grid):
+        self.current_grid = current_grid
+        self.cols = cols
+        self.rows = rows
 
 
 
-            #count number of neighbors for current cell
+    #interates through array grid and determines what the next grid will look like according to rules of game of life
+    def create_next_grid(self):
+        #next grid array
+        next_grid: boolean = [[False for i in range(self.cols)] for j in range(self.rows)]
 
-            #left cell
-            if (j - 1) >= 0:
-                if current_grid[i][j-1] == True:
-                    neighbor_count += 1
-            #top-left cell
-            if (j - 1) >= 0 and (i - 1) >= 0:
-                if current_grid[i-1][j-1] == True:
-                    neighbor_count += 1
-            #top cell
-            if (i - 1) >= 0:
-                if current_grid[i-1][j] == True:
-                    neighbor_count += 1
-            #top-right cell
-            if (i - 1) >= 0 and (j + 1) < cols:
-                if current_grid[i-1][j+1] == True:
-                    neighbor_count += 1
-            #right cell
-            if (j + 1) < cols:
-                if current_grid[i][j+1] == True:
-                    neighbor_count += 1
-            #bottom-right cell
-            if (j + 1) < cols and (i + 1) < rows:
-                if current_grid[i+1][j+1] == True:
-                    neighbor_count += 1
-            #bottom cell
-            if (i + 1) < rows:
-                if current_grid[i+1][j] == True:
-                    neighbor_count += 1
-            #bottom-left cell
-            if (i + 1) < rows and (j - 1) > 0:
-                if current_grid[i+1][j-1]:
-                    neighbor_count += 1
+        for i in range(len(self.current_grid)):
+            for j in range(len(self.current_grid[i])):
+                #number of neighbors cell has
+                neighbor_count: int = 0
+                #current status of cell
+                status: boolean = self.current_grid[i][j]
 
 
-            #determine status of current cell in next grid
 
-            #if cell is alive has 2 neighbors, it lives
-            if neighbor_count == 2 and status == True:
-                next_grid[i][j] == True
-                #if cell has 3 neighbors, it lives regardless whether it was alive before or now
-            elif neighbor_count == 3:
-                next_grid[i][j] == True
-            #else cell dies
-            else:
-                next_grid[i][j] == False
+                #count number of neighbors for current cell
 
-    #returns the next grid
-    return next_grid
+                #left cell
+                if (j - 1) >= 0:
+                    if self.current_grid[i][j-1] == True:
+                        neighbor_count += 1
+                #top-left cell
+                if (j - 1) >= 0 and (i - 1) >= 0:
+                    if self.current_grid[i-1][j-1] == True:
+                        neighbor_count += 1
+                #top cell
+                if (i - 1) >= 0:
+                    if self.current_grid[i-1][j] == True:
+                        neighbor_count += 1
+                #top-right cell
+                if (i - 1) >= 0 and (j + 1) < self.cols:
+                    if self.current_grid[i-1][j+1] == True:
+                        neighbor_count += 1
+                #right cell
+                if (j + 1) < self.cols:
+                    if self.current_grid[i][j+1] == True:
+                        neighbor_count += 1
+                #bottom-right cell
+                if (j + 1) < self.cols and (i + 1) < self.rows:
+                    if self.current_grid[i+1][j+1] == True:
+                        neighbor_count += 1
+                #bottom cell
+                if (i + 1) < self.rows:
+                    if self.current_grid[i+1][j] == True:
+                        neighbor_count += 1
+                #bottom-left cell
+                if (i + 1) < self.rows and (j - 1) > 0:
+                    if self.current_grid[i+1][j-1]:
+                        neighbor_count += 1
+
+
+
+
+                #determine status of current cell in next grid
+                
+                #if cell is alive has 2 neighbors, it lives
+                if neighbor_count == 2 and status == True:
+                    
+                    next_grid[i][j] = True
+                    #if cell has 3 neighbors, it lives regardless whether it was alive before or now
+                elif neighbor_count == 3:
+                    next_grid[i][j] = True
+                #else cell dies
+                else:
+                    next_grid[i][j] = False
+       
+        self.current_grid = next_grid
+        
 
--- user_interface.py
+++ user_interface.py
@@ -2,10 +2,51 @@ from life import *
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="hello world").grid(column=0, row=0)
-ttk.Button(frm, text="quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+cols = 10
+rows = 10
 
+grid = [[False for i in range(cols)] for j in range(rows)]
+
+
+grid[0][0] = True
+grid[1][1] = True
+grid[2][2] = True
+grid[3][3] = True
+grid[3][4] = True
+grid[4][4] = True
+
+
+print(grid)
+
+
+life = Life(cols, rows, grid)
+
+
+
+life.create_next_grid()
+
+print(life.current_grid)
+
+
+
+root = Tk()
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+
+
+#example values
+for x in range(10):
+    for y in range(10):
+        btn = Button(root)
+        btn.grid(column=x, row=y, sticky="news")
+
+root.columnconfigure(tuple(range(cols)), weight=1)
+root.rowconfigure(tuple(range(rows)), weight=1)
+
+def SEND():
+    
+
+    root.after(500, SEND)
+
+SEND()
+root.mainloop()
